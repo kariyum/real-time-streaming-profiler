94973a4f219286fa4c4d598578337e6dd842b68e
Karim Ben Amara <karimbenamara91@gmail.com>
2025-11-08 23:01:57 +0100

    add expand and collapse buttons for all rows

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -2,6 +2,27 @@
 	import Table from './Table.svelte';
 	import { processData, computeChildren } from '../utils.ts';
 	import type { EnhancedMetric, SingleMetric } from '$lib/types.ts';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { RowToggleEvent } from '$lib/event.ts';
+
+	let initShow: boolean = $state(false);
+	let toggleEvent: RowToggleEvent;
+	onMount(() => {
+		if (browser) {
+			toggleEvent = RowToggleEvent.getInstance();
+		}
+	});
+
+	function expand() {
+		toggleEvent.toggleOn();
+		initShow = true;
+	}
+
+	function collapse() {
+		toggleEvent.toggleOff();
+		initShow = false;
+	}
 
 	let eventSource: EventSource | undefined = $state(undefined);
 	let ip = $state('localhost');
@@ -12,65 +33,74 @@
 	let obsolete = $state(false);
 
 	let metrics: SingleMetric[] = $state([]);
-	metrics = [
-		{
-			id: 'test01',
-			parent: null,
-			start_end_times: [1 * 1000000, 6 * 1000000]
-		},
-		{
-			id: 'test03',
-			parent: null,
-			start_end_times: [1 * 1000000, 6 * 1000000]
-		},
-		{
-			id: 'test01',
-			parent: null,
-			start_end_times: [1 * 1000000, 7 * 1000000]
-		},
-		{
-			id: 'test02',
-			parent: 'test01',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test022',
-			parent: 'test01',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test02',
-			parent: 'test01',
-			start_end_times: [4 * 1000000, 6 * 1000000]
-		},
-		{
-			id: 'test05',
-			parent: 'test02',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test06',
-			parent: 'test05',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test07',
-			parent: 'test02',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test08',
-			parent: 'test06',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		},
-		{
-			id: 'test10',
-			parent: 'test08',
-			start_end_times: [3 * 1000000, 5 * 1000000]
-		}
-	];
+	// metrics = [
+	// 	{
+	// 		id: 'test01',
+	// 		parent: null,
+	// 		start_end_times: [1 * 1000000, 6 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test03',
+	// 		parent: null,
+	// 		start_end_times: [1 * 1000000, 6 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test01',
+	// 		parent: null,
+	// 		start_end_times: [1 * 1000000, 7 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test02',
+	// 		parent: 'test01',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test022',
+	// 		parent: 'test01',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test02',
+	// 		parent: 'test01',
+	// 		start_end_times: [4 * 1000000, 6 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test05',
+	// 		parent: 'test02',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test06',
+	// 		parent: 'test05',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test07',
+	// 		parent: 'test02',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test08',
+	// 		parent: 'test06',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	},
+	// 	{
+	// 		id: 'test10',
+	// 		parent: 'test08',
+	// 		start_end_times: [3 * 1000000, 5 * 1000000]
+	// 	}
+	// ];
 
-	enhancedMetrics = computeChildren(processData(metrics));
+	// setInterval(() => {
+	// 	metrics.push({
+	// 		id: 'test0' + Math.floor(Math.random() * 10).toString(),
+	// 		parent: null,
+	// 		start_end_times: [Math.random() * 10 * 1000000, Math.random() * 10 * 1000000]
+	// 	});
+	// 	enhancedMetrics = computeChildren(processData(metrics));
+	// }, 500);
+
+	// enhancedMetrics = computeChildren(processData(metrics));
 	function connect() {
 		if (eventSource) {
 			eventSource.close();
@@ -136,6 +166,8 @@
 		}}>Reset</button
 	>
 	<button onclick={disconnect}>Disconnect</button>
+	<button onclick={() => expand()} aria-label="expand"> Expand</button>
+	<button onclick={() => collapse()} aria-label="collapse">Collapse</button>
 	<h1>
 		{#if connected == 0}
 			Connecting... ⌛ to {ip}
@@ -147,21 +179,13 @@
 			<span style="color:red;">ERROR</span> 😱😱😱
 		{/if}
 	</h1>
-	<Table
-		data={enhancedMetrics}
-		max={globalMax}
-		min={globalMin}
-		globalShowChildren={toggleChildren}
-	/>
+	<Table data={enhancedMetrics} max={globalMax} min={globalMin} {initShow} />
 	<button
 		onclick={() => {
 			navigator.clipboard
 				.writeText(JSON.stringify(enhancedMetrics))
 				.then(() => {
-					// Provide user feedback
 					buttonLabel = 'Copied!';
-
-					// Optional: revert the button text after a few seconds
 					setTimeout(() => {
 						buttonLabel = 'Copy!';
 					}, 2000);
