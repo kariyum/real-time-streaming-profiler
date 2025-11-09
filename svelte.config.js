e4dca37d1374be7568db8369c3f2d4c140accaed
Karim Ben Amara <karimbenamara91@gmail.com>
2025-11-09 22:32:41 +0100

    handle unseen routes while prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,13 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		prerender: {
+			handleUnseenRoutes: "warn"
+		},
+
+		adapter: adapter()
+	}
 };
 
 export default config;
