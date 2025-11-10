7b7fef092662f1b91244853553d484fc7ab550f8
Karim Ben Amara <karim.benamara@cognira.com>
2025-11-10 12:27:07 +0100

    prerendering

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -2,14 +2,19 @@
 	import { page } from '$app/state';
 	import Dashboards from '$lib/components/Dashboards.svelte';
 	import TableWithMinMax from '$lib/components/TableWithMinMax.svelte';
+	import { dashboardsRepo, Database, type DashboardEntity } from '$lib/db';
+	import { onMount } from 'svelte';
+	let dashboards: DashboardEntity[] = [];
+	let linkData: string | null = null;
 
-	let { data } = $props();
-
-	let dashboards = $derived(data.dashboards);
-	let linkData = page.url.searchParams.get('data');
+	onMount(async () => {
+		const db = (await Database.getInstance()).db;
+		dashboards = db ? await dashboardsRepo.getAllDashboards(db) : [];
+		linkData = page.url.searchParams.get('data');
+	});
 </script>
 
-{#if linkData !== null}
+{#if typeof linkData == 'string'}
 	<TableWithMinMax data={linkData}></TableWithMinMax>
 {:else}
 	<Dashboards {dashboards}></Dashboards>
